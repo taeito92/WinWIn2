4dd5bee8d8d39cf1c2bd0a71b50f61bdeeab8f58
taeito92 <bethest4r@gmail.com>
2021-07-09 16:01:25 +0900

    asd

---
+++ app/src/main/java/com/knk/insa/Insa.java
@@ -0,0 +1,46 @@
+package com.knk.insa;
+
+public class Insa implements InsaM {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void insert() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void select() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void ser() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void up() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void del() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void order() {
+    // TODO Auto-generated method stub
+
+  }
+
+}
